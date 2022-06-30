35cc2c77087f14ef0c703829f4880e010d1711cd
ksh033 <94456671@qq.com>
2022-06-30 21:05:26 +0800

    test

---
+++ .fatherrc.ts
@@ -0,0 +1,25 @@
+import { readdirSync } from 'fs';
+import { join } from 'path';
+
+// utils must build before core
+// runtime must build before renderer-react
+// components dependencies order: form -> table -> list
+const headPkgs: string[] = ['element'];
+const tailPkgs = readdirSync(join(__dirname, 'packages')).filter(
+  (pkg) => pkg.charAt(0) !== '.' && !headPkgs.includes(pkg),
+);
+
+export default {
+  cjs: { type: 'babel', lazy: true },
+  esm: {
+    type: 'babel',
+    importLibToEs: true,
+  },
+  cssModules: true,
+
+  pkgs: ['clientplugin'],
+  extraBabelPlugins: [
+    ['babel-plugin-import', { libraryName: 'antd', libraryDirectory: 'es', style: true }, 'antd'],
+    [require('./scripts/replaceLib')],
+  ],
+};
